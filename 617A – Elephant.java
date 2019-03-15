aa7c6c418c36ba35c3d51eb4c885ffc256ec9e73
Yehia Ezzat <20699999+YehiaAE@users.noreply.github.com>
2019-03-15 15:42:51 +0200

    Create 617A – Elephant.java

---
+++ 617A – Elephant.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+/**
+ *
+ * @author Yehia Ezzat
+ */
+public class Elephant {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        System.out.println((x + 4) / 5);
+    }
+}
