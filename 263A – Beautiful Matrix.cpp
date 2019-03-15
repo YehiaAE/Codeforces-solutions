713623e8046fdf6ab9a323e26c327a66829359ea
Yehia Ezzat <20699999+YehiaAE@users.noreply.github.com>
2019-03-15 15:29:51 +0200

    Create 263A – Beautiful Matrix.cpp

---
+++ 263A – Beautiful Matrix.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int arr[5][5],i , j ;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            cin>>arr[i][j];
+            if(arr[i][j]!=0)
+            {
+                cout<<abs(i-2)+abs(j-2);
+                return 0 ;
+            }
+        }
+    }
+}
